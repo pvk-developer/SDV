80a6c895e66125a71560c4d6e6e9a1d06f01859e
Plamen Valentinov <pvkdeveloper@gmx.com>
2019-10-29 15:56:03 +0100

    wip

--- sdv/__init__.py
+++ sdv/__init__.py
@@ -8,6 +8,12 @@ __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
 __version__ = '0.1.3-dev'
 
+
+import json
+import os
+
+import pandas as pd
+
 from sdv.metadata import Metadata
 from sdv.modeler import Modeler
 from sdv.sampler import Sampler
@@ -19,3 +25,23 @@ __all__ = (
     'SDV',
     'Sampler',
 )
+
+
+def get_demo():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = os.path.abspath(os.path.join(dir_path, '../examples/readme_demo'))
+
+    tables = {
+        'users': pd.read_csv(os.path.join(dir_path, 'users.csv')),
+        'sessions': pd.read_csv(os.path.join(dir_path, 'sessions.csv')),
+    }
+
+    transactions = pd.read_csv(os.path.join(dir_path, 'transactions.csv'))
+    transactions['datetime'] = pd.to_datetime(transactions['datetime'], format='%Y-%m-%d')
+
+    tables['transactions'] = transactions
+
+    with open(os.path.join(dir_path, 'metadata.json')) as metadata:
+        metadata = json.load(metadata)
+
+    return metadata, tables
